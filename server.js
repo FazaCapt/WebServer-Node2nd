ca09d450a20e40cad2fa2724721e948195db5482
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-04-29 21:50:11 +0700

    Setup start and haroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -18,9 +19,9 @@ app.use((req, res, next) => {
   next();
 })
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-})
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// })
 
 hbs.registerHelper('getCurrentlyYear', () => {
   return new Date().getFullYear()
@@ -48,6 +49,6 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('server on 3000');
+app.listen(port, () => {
+  console.log(`server on ${port}`);
 });
